b8cdbb723e5b936df8aff2095f3c90803fe1b368
崔彦强 <1764067223@qq.com>
2020-08-15 11:51:56 +0800

    首页banner区域

---
+++ pages/component/home/domain/domain.js
@@ -0,0 +1,34 @@
+// pages/component/home/domain/domain.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    domainlist:[
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'1分拼团'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'秒杀'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'笔记本'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'家电'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'生活'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'企业购'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'电子产品'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'智能'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'出行'},
+      {src:'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37dc0898c2624b4130da4631f2d9a675.jpg?thumb=1&w=234&h=300&f=webp&q=90',title:'旅游'},
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/component/home/potation/potation.js
@@ -0,0 +1,32 @@
+// pages/component/potation.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500,
+    imglist:[
+      'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/29609adf7341c030283bb00268a03eea.jpg?thumb=1&w=1226&h=460&f=webp&q=90',
+      'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/fe253b0048a5517abca36a605acacba3.jpg?thumb=1&w=1226&h=460&f=webp&q=90',
+      'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/b7c2e5c62dfcaae720a3cd909c0eae52.jpg?w=2452&h=920'
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
